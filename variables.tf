988e62126fc669b1479ff19c8e517476e0d7571e
jsredmond <norrinscott@gmail.com>
2023-01-16 12:40:17 -0500

    finished iam roles

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "env" {
   type    = string
   default = "prod"
 }
+
+variable "cloudtrail" {
+  type = string
+  default = "mycloudtrails3bucket"
+}
+
+variable "ctkey" {
+  type = string
+  default = "us-east-1:accountid:key/mykmsidforencryptingcloudtrail"
+}
